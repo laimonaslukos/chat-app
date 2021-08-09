d608b90f127bb18e76a179d67d5e945f80a1ab18
„Laimonas <laimonas.lukosevicius@gmail.com>
2021-08-09 22:24:10 +0300

    Add LoginForm.jsx

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
-import {ChatEngine} from "react-chat-engine" ;
-import "./App.css"
-import ChatFeed from "./components/ChatFeed"
+import { ChatEngine } from 'react-chat-engine';
 
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+import './App.css';
 
+const projectID = '39dd2ac6-3958-451e-b5b9-2e135be196f1';
 
 const App = () => {
-  return (
-    <ChatEngine 
-    height = "100vh"
-    projectID = "39dd2ac6-3958-451e-b5b9-2e135be196f1"
-    userName = "Laimonas"
-    userSecret = "123"
-    renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
-    />
-  )
-}
+  if (!localStorage.getItem('username')) return <LoginForm />;
 
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
+  );
+};
+
+// infinite scroll, logout, more customizations...
 
 export default App;
